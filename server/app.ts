99622e27fc9bff92a7be9ece7dfaf7d14a0355a2
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-07-19 21:26:23 +0900

    add read

--- server/app.ts
+++ server/app.ts
@@ -7,6 +7,13 @@ import cors from "cors";
 const app = express();
 
 import reviewRouter from "./src/routes/review";
+import programRouter from "./src/routes/program";
+import priceRouter from "./src/routes/price";
+import machineRouter from "./src/routes/machine";
+import hoursRouter from "./src/routes/hours";
+import facilityRouter from "./src/routes/facility";
+import companyRouter from "./src/routes/company";
+import commentRouter from "./src/routes/comment";
 
 app.use(express.json());
 app.use(morgan("tiny"));
@@ -15,6 +22,13 @@ app.use(helmet());
 app.use(cors());
 
 app.use("/review", reviewRouter);
+app.use("/program", programRouter);
+app.use("/price", priceRouter);
+app.use("/machine", machineRouter);
+app.use("/hours", hoursRouter);
+app.use("/facility", facilityRouter);
+app.use("/company", companyRouter);
+app.use("/comment", commentRouter);
 
 app.use("/", (req: Request, res: Response) => {
     res.status(404).send("Anavailable route");
